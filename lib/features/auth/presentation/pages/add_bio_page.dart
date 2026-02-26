e3cc982fed5825bce8ad4f710ff82a4cad11fc93
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-02-26 13:45:36 +0530

    form validations

--- lib/features/auth/presentation/pages/add_bio_page.dart
+++ lib/features/auth/presentation/pages/add_bio_page.dart
@@ -13,6 +13,15 @@ class AddBioPage extends StatefulWidget {
 
 class _AddBioPageState extends State<AddBioPage> {
   final TextEditingController _bioController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  double _opacity = 0.5;
+  void _checkFormValidity() {
+    final isValid = _bioController.text.isNotEmpty;
+    setState(() {
+      _opacity = isValid ? 1 : 0.5;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,30 +29,39 @@ class _AddBioPageState extends State<AddBioPage> {
         child: Center(
           child: SingleChildScrollView(
             padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SvgPicture.asset("assets/logo.svg", height: 150),
-                SizedBox(height: 24),
-                Text(
-                  "Add Bio .",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SizedBox(height: 24),
-                CommonTextField(
-                  controller: _bioController,
-                  hintText: "Say something about yourself to us",
-                  label: "Bio",
-                  maxLines: 2,
-                ),
-                SizedBox(height: 24),
-                CommonButton(
-                  buttonName: "Sign Up .",
-                  onTap: () {
-                    // context.push("/");
-                  },
-                ),
-              ],
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset("assets/logo.svg", height: 150),
+                  SizedBox(height: 24),
+                  Text(
+                    "Add Bio .",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  SizedBox(height: 24),
+                  CommonTextField(
+                    controller: _bioController,
+                    hintText: "Say something about yourself to us",
+                    label: "Bio",
+                    maxLines: 2,
+                    onChanged: (value) => _checkFormValidity(),
+                  ),
+                  SizedBox(height: 24),
+                  AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: Duration(milliseconds: 500),
+                    child: CommonButton(
+                      buttonName: "Sign Up .",
+                      onTap: () {
+                        if(formKey.currentState!.validate()){}
+                        // context.push("/");
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -15,6 +15,23 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  double _opacity = 0.5;
+  void _checkFormValidity() {
+    final isValid =
+        emailController.text.isNotEmpty && passwordController.text.isNotEmpty;
+    setState(() {
+      _opacity = isValid ? 1 : 0.5;
+    });
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,55 +39,72 @@ class _LoginPageState extends State<LoginPage> {
         child: Center(
           child: SingleChildScrollView(
             padding: EdgeInsetsGeometry.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SvgPicture.asset("assets/logo.svg", height: 100),
-                SizedBox(height: 24),
-                Text("Login .", style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(height: 24),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset("assets/logo.svg", height: 100),
+                  SizedBox(height: 24),
+                  Text(
+                    "Login .",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  SizedBox(height: 24),
 
-                CommonTextField(
-                  controller: emailController,
-                  hintText: "enter your email",
-                  label: "Email",
-                ),
-                SizedBox(height: 24),
-                CommonTextField(
-                  controller: passwordController,
-                  hintText: "enter your password",
-                  label: "Password",
-                  isObscureText: true,
-                ),
-                SizedBox(height: 24),
-                CommonButton(buttonName: "Login .", onTap: () {
-                  
-                }),
-                SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
+                  CommonTextField(
+                    controller: emailController,
+                    hintText: "enter your email",
+                    label: "Email",
+                    onChanged: (value) => _checkFormValidity(),
+                  ),
+                  SizedBox(height: 24),
+                  CommonTextField(
+                    controller: passwordController,
+                    hintText: "enter your password",
+                    label: "Password",
+                    isObscureText: true,
+                    onChanged: (value) => _checkFormValidity(),
+                  ),
+                  SizedBox(height: 24),
+                  AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: Duration(milliseconds: 500),
+                    child: CommonButton(
+                      buttonName: "Login .",
                       onTap: () {
-                        context.push("/signup");
+                        if (formKey.currentState!.validate()) {
+                          
+                        }
                       },
-                      child: RichText(
-                        text: TextSpan(
-                          text: "Don't have an account ? ",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                          children: [
-                            TextSpan(
-                              text: "SignUp",
-                              style: Theme.of(context).textTheme.bodyMedium!
-                                  .copyWith(color: AppColors.appColor),
-                            ),
-                          ],
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          context.push("/signup");
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Don't have an account ? ",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                            children: [
+                              TextSpan(
+                                text: "SignUp",
+                                style: Theme.of(context).textTheme.bodyMedium!
+                                    .copyWith(color: AppColors.appColor),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
