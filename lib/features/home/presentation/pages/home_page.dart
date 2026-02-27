7b67da9ae1dd215842a6bcba71ea4b347213c1d6
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-02-27 13:48:44 +0530

    search tab

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:wechat/core/utils/snackbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,7 +25,7 @@ class _HomePageState extends State<HomePage> {
             ),
             child: SvgPicture.asset(
               "assets/icons/profile.svg",
-              width: 16,
+              width: 14,
               colorFilter: ColorFilter.mode(
                 Theme.of(context).colorScheme.secondary,
                 BlendMode.srcIn,
@@ -39,35 +38,65 @@ class _HomePageState extends State<HomePage> {
         actions: [
           Center(
             child: Container(
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.all(8),
               margin: EdgeInsets.only(left: 16),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: Theme.of(context).colorScheme.surfaceContainer,
               ),
-              child: SvgPicture.asset(
-                "assets/icons/notification.svg",
-                width: 20,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.secondary,
-                  BlendMode.srcIn,
-                ),
+              child: Icon(
+                Icons.notifications_none,
+                size: 20,
+                color: Theme.of(context).colorScheme.secondary,
               ),
             ),
           ),
         ],
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            // Text("Home Page")
-            ElevatedButton(
-              onPressed: () {
-                showSnackabr(context, "Login Success");
-              },
-              child: Text("press me"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Theme.of(context).colorScheme.surfaceContainer,
+                ),
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/icons/search.svg",
+                      width: 16,
+                      colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.secondary,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search People",
+                          hintStyle: Theme.of(context).textTheme.bodySmall,
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          isDense: true,
+                          contentPadding: EdgeInsets.zero,
+                        ),
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
